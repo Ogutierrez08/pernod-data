7928c442c2ebaaf04b1851bb6e69f8b502e37026
Gneric <46692513+Gneric@users.noreply.github.com>
2019-06-13 11:59:01 -0500

    Mailer w/ Table

--- util/mailerFactory.js
+++ util/mailerFactory.js
@@ -1,16 +1,5 @@
 module.exports = {
-    envioData: (array, onSuccess) => {
-        var data = array.map( Element => {
-            return ("CODIGO DISTRIBUIDOR: "+ Element.CODIGODISTRIBUIDOR
-                  + '|'
-                  +"CODIGO PRODUCTO: "+ Element.CODIGOPRODUCTODISTRIBUIDOR
-                  + '|'
-                  +"DESCRIPCION: "+ Element.DESCRIPCION
-                  + '|'
-                  +"CODIGOEAN: "+ Element.CODIGOEAN
-                  ).split('|') 
-        }).join('') + '\n'
-        
+    envioData: (array, onSuccess) => {               
         var nodemailer = require('nodemailer')
         let transport = nodemailer.createTransport({
             service: 'gmail',
@@ -19,19 +8,49 @@ module.exports = {
                 pass: 'Invernalia!OB19'
             }
         })
-        
         var msg = {
             from: 'oscar.biondi.vidasoft@gmail.com',
             to: 'oscar.biondi.vidasoft@gmail.com',
-            subject: 'dataJS Pernod',
+            subject: 'TESTING ONLY MAILER',
             text: 'Tabla',
-            html: `<p>${data}</p>`
-            
+            html: `<b>Los siguientes productos no tienen homologacion: </b><br/><br/><br/>`            
         }
-        transport.sendMail(msg, (err) => {
-            if(err) console.log(err)
-            onSuccess()
-        })
+        if(array.length > 0){
+            var tableBody = "<table style='width:100%; border: 1px solid black; border-collapse: collapse;'><tr>";
+            tableBody += "<th style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>DISTRIBUIDOR</th>"
+            tableBody += "<th style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>PRODUCTO</th>"
+            tableBody += "<th style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>DESCRIPCION</th>"
+            tableBody += "<th style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>EAN</th></tr>"
+            for(var i=0; i<array.length; i++){
+                tableBody += "<tr>";
+                tableBody += "<td style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>" + array[i].CODIGODISTRIBUIDOR + "</td>"
+                tableBody += "<td style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>" + array[i].CODIGOPRODUCTODISTRIBUIDOR + "</td>"
+                tableBody += "<td style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>" + array[i].DESCRIPCION + "</td>"
+                tableBody += "<td style='border: 1px solid black; border-collapse: collapse; padding: 5px; text-align: left;'>" + array[i].CODIGOEAN + "</td>"
+                tableBody += "</tr>";
+            }
+            tableBody += "</table><br/><br/>"
+            msg.html += tableBody
+
+            transport.sendMail(msg, (err) => {
+                if(err) {
+                    msg.subject = 'ERROR EN MAILER PERNOD'
+                    msg.html = `${err}`
+                }
+                onSuccess()
+            })
+        }
+        else {
+            msg.subject = 'Data Pernod no cuenta con productos no Homologados'
+            msg.html = '<b>No hay productos que les falte CODIGO PERNOD</b><br/>'
+            transport.sendMail(msg, (err) => {
+                if(err) {
+                    msg.subject = 'ERROR EN MAILER PERNOD'
+                    msg.html = `${err}`
+                }
+                onSuccess()
+            })
+        }
     }
 }
 
